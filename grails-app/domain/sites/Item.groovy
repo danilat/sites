651b2e5212f73ad66618b10338a2aa5f57a707c0
Dani Latorre <danilat83@gmail.com>
2012-02-16 12:46:58 +0100

    implemented localizable

--- grails-app/domain/sites/Item.groovy
+++ grails-app/domain/sites/Item.groovy
@@ -1,24 +1,32 @@
 package sites
 
 class Item {
+    transient def geocodingService
     static belongsTo = [category: Category]
-	String name
-	String description
-	String place
-	
-	Double lat
-	Double lng
-	
-	static hasMany = [photo: Photo]
+    String name
+    String description
+    String place
+    
+    Double lat
+    Double lng
+    
+    static hasMany = [photo: Photo]
     static constraints = {
-		name()
-		description()
-		place()
-		lat(nullable:true)
-		lng(nullable:true)
+        name()
+        description()
+        place()
+        lat(nullable:true)
+        lng(nullable:true)
     }
 
-	static mapping = {
-		description type: 'text'
-	}
+    static mapping = {
+        description type: 'text'
+    }
+    
+    def localize(){
+        def latLng = geocodingService.findLatLngByAddress(place)
+        lat = latLng.lat
+        lng = latLng.lng
+        this
+    }
 }
--- test/unit/sites/ItemTests.groovy
+++ test/unit/sites/ItemTests.groovy
@@ -5,13 +5,24 @@ package sites
 import grails.test.mixin.*
 import org.junit.*
 
+import groovy.mock.interceptor.*
+
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
 @TestFor(Item)
 class ItemTests {
 
-    void testSomething() {
-       fail "Implement me"
+    void testLocalizeAnAdress() {
+       def item = new Item(place: "San Esteban de Litera")
+       def geocodingServiceMock = new MockFor(com.grails.plugins.localizable.GeocodingService)
+       geocodingServiceMock.demand.findLatLngByAddress(){
+           [lat:1, lng:2]
+       }
+       item.geocodingService = geocodingServiceMock.proxyInstance()
+       item.localize()
+       
+       assertNotNull item.lat
+       assertNotNull item.lng
     }
 }
